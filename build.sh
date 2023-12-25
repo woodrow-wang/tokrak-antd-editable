102dc7eb2f1f88ef8042b30f802fc4afbb386268
woodrow_tokrak_ide <woodrow@tokrak_ide>
2023-12-25 08:59:04 +0800

    modify package.json

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 yarn
+yarn tsc -v
+yarn tsc
+yarn rc-tools run compile --babel-runtime
+$env:NODE_OPTIONS="--openssl-legacy-provider"
 export NODE_OPTIONS=--openssl-legacy-provider
-yarn build
+yarn rc-tools run build
+yarn rc-tools run server
+yarn publish
